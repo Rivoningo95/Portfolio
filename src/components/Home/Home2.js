1994088ed99462994cf61fa508331030dc10adfd
Evidence <evidence.chauke@dadevs.co.za>
2022-11-21 15:11:41 +0200

    adding information

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -30,8 +30,8 @@ function Home2() {
               <br />
               My field of Interest's are building new &nbsp;
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
+                <b className="purple">Application alone </b> and
+                also to learn more about technolgy{" "}
                 <b className="purple">
                   Deep Learning and Natural Launguage Processing.
                 </b>
@@ -67,7 +67,7 @@ function Home2() {
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/Rivoningo95"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -77,7 +77,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/RirieyC12"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/mwlite/in/evidence-rivoningo-chauke-021b3b227"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/https://www.instagram.com/mavondzo_bosslady/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
