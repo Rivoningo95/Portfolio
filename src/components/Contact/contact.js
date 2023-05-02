8b7966ac7fc9d77d4cd968c493d2de21c31fea6c
Evidence <mashambarm26@gmail.com>
2023-05-02 14:06:23 +0200

    busy with contact me

---
+++ src/components/Contact/contact.js
@@ -0,0 +1,71 @@
+import "./contact.css";
+import React, { Component }  from 'react';
+import emailjs from 'emailjs-com';
+import { useRef } from 'react';
+
+function Contact() {
+    const form = useRef();
+    
+  return (
+    <div class="container">
+      <div class="row">
+        <div class="col align-self-center">
+          <h1 class="text-center">Email - JavaScript Contact Form</h1>
+          {/* <!-- contact form --> */}
+          <form>
+            {/* <!-- name --> */}
+            <div class="form-group">
+              <label for="name">Name</label>
+              <input
+                type="name"
+                name="name"
+                class="form-control"
+                id="name"
+                placeholder="enter your name"
+              />
+            </div>
+
+            {/* <!-- email --> */}
+            <div class="form-group">
+              <label for="email">Email address</label>
+              <input
+                type="email"
+                name="email"
+                class="form-control"
+                id="email"
+                placeholder="enter your email"
+              />
+            </div>
+
+            {/* <!-- subject --> */}
+            <div class="form-group">
+              <label for="subject">Subject</label>
+              <input
+                type="text"
+                name="subject"
+                class="form-control"
+                id="subject"
+                placeholder="enter email subject"
+              />
+            </div>
+
+            <div class="form-group">
+              <label for="email_body">Message</label>
+              <textarea
+                class="form-control"
+                id="email_body"
+                rows="5"
+              ></textarea>
+            </div>
+
+            <button type="submit" class="btn btn-primary">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
